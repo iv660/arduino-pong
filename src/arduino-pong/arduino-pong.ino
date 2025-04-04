17a923e93e267c1c6e092e0e444df05cec0aeba0
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2025-04-05 00:43:57 +0300

    feat: Prepare project for Entity-Component-System architecture

--- src/arduino-pong/arduino-pong.ino
+++ src/arduino-pong/arduino-pong.ino
@@ -1,15 +1,31 @@
+#include <Vector.h>
 #include <ApplianceFactory.h>
 #include <Appliance.h>
 
 using XC::Hardware::Appliance;
 using XC::SlimPad::ApplianceFactory;
 
+class Entity {};
+class PositionComponent {};
+class VelocityComponent {};
+class MovementSystem {};
+
 ApplianceFactory applianceFactory;
 Appliance appliance;
+
+Vector<Entity> entities;
+
+Entity leftPaddle;
+Entity rightPaddle;
+Entity ball;
 
 void setup() {
     applianceFactory.useJoystick().asAnalogJoystick();
     appliance = applianceFactory.createAppliance();
+
+    entities.push_back(leftPaddle);
+    entities.push_back(rightPaddle);
+    entities.push_back(ball);
 }
 
 void loop() {
