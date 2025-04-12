f4bb02daed70dc79fdde10b45101aace626cf173
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2025-04-12 17:34:53 +0300

    refact: Extract methods for bouncing functionality

--- src/arduino-pong/CollisionSystem.cpp
+++ src/arduino-pong/CollisionSystem.cpp
@@ -1,16 +1,65 @@
 #include "CollisionSystem.h"
 
+bool CollisionSystem::movesLeft(Entity * entity)
+{
+    return false;
+}
+
+bool CollisionSystem::movesRight(Entity * entity)
+{
+    return entity->movementComponent.xMovement.velocity > 0;
+}
+
+bool CollisionSystem::movesUp(Entity * entity)
+{
+    return false;
+}
+
+bool CollisionSystem::movesDown(Entity * entity)
+{
+    return false;
+}
+
+bool CollisionSystem::hitsFromLeft(Entity * entity1, Entity * entity2)
+{
+    return entity1->positionComponent.position.x + entity1->bouncingBoxComponent.width >= entity2->positionComponent.position.x - 1;
+}
+
+bool CollisionSystem::hitsFromRight(Entity * entity1, Entity * entity2)
+{
+    return false;
+}
+
+bool CollisionSystem::hitsFromTop(Entity * entity1, Entity * entity2)
+{
+    return false;
+}
+
+bool CollisionSystem::hitsFromBottom(Entity * entity1, Entity * entity2)
+{
+    return false;
+}
+
+void CollisionSystem::bounceVertically(Entity *entity)
+{
+    entity->movementComponent.yMovement.velocity *= -1;
+}
+
+void CollisionSystem::bounceHorizontally(Entity *entity)
+{
+    entity->movementComponent.xMovement.velocity *= -1;
+}
+
 void CollisionSystem::update(Vector<BouncingBoxComponent> bouncingBoxComponents)
 {
     for (int i = 0; i < bouncingBoxComponents.size(); i++) {
         for (int j = i + 1; j < bouncingBoxComponents.size(); j++) {
-            BouncingBoxComponent bouncable = bouncingBoxComponents[i];
-            BouncingBoxComponent surface = bouncingBoxComponents[j];
+            Entity *bouncable1 = bouncingBoxComponents[i].entity;
+            Entity *bouncable2 = bouncingBoxComponents[j].entity;
 
-            if (bouncable.entity->movementComponent.xMovement.velocity > 0
-                && bouncable.entity->positionComponent.position.x + bouncable.width >= surface.entity->positionComponent.position.x - 1
-            ) {
-                bouncable.entity->movementComponent.xMovement.velocity *= -1;
+            if (movesRight(bouncable1) && hitsFromLeft(bouncable1, bouncable2)) {
+                bounceHorizontally(bouncable1);
+                bounceHorizontally(bouncable2);
             }
         }
     }
--- src/arduino-pong/CollisionSystem.h
+++ src/arduino-pong/CollisionSystem.h
@@ -6,6 +6,19 @@
 
 class CollisionSystem
 {
+    private:
+        bool movesLeft(Entity *entity);
+        bool movesRight(Entity *entity);
+        bool movesUp(Entity *entity);
+        bool movesDown(Entity *entity);
+
+        bool hitsFromLeft(Entity * entity1, Entity * entity2);
+        bool hitsFromRight(Entity * entity1, Entity * entity2);
+        bool hitsFromTop(Entity * entity1, Entity * entity2);
+        bool hitsFromBottom(Entity * entity1, Entity * entity2);
+        
+        void bounceVertically(Entity *entity);
+        void bounceHorizontally(Entity *entity);
     public:
         void update(Vector<BouncingBoxComponent> bouncingBoxComponents);
 };
