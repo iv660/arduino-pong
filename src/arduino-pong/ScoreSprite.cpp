54e13f8573d0bf94ecc94f58673011b57ed397b7
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2025-05-09 01:55:27 +0300

    feat: Add score placeholder text

--- src/arduino-pong/ScoreSprite.cpp
+++ src/arduino-pong/ScoreSprite.cpp
@@ -1,17 +1,24 @@
 #include "ScoreSprite.h"
 
+char * ScoreSprite::getScoreString()
+{
+    return "00 00";
+}
+
 void ScoreSprite::drawOn(ScreenInterface* screen, Position position)
 {
     screen->stroke(200, 200, 200);
     screen->fill(200, 200, 200);
-    screen->rect(position.x, position.y, width, height);
+    screen->textSize(fontSize);
+    screen->text(getScoreString(), position.x, position.y);
 }
 
 void ScoreSprite::eraseFrom(ScreenInterface* screen, Position position)
 {
     screen->stroke(0, 0, 0);
     screen->fill(0, 0, 0);
-    screen->rect(position.x, position.y, width, height);
+    screen->textSize(fontSize);
+    screen->text(getScoreString(), position.x, position.y);
 }
 
 int ScoreSprite::getWidth()
--- src/arduino-pong/ScoreSprite.h
+++ src/arduino-pong/ScoreSprite.h
@@ -8,8 +8,11 @@
 class ScoreSprite: public SpriteInterface
 {
     private: 
-        int const width = 40;
-        int const height = 12;
+        int const width = 30;
+        int const height = 10;
+        int const fontSize = 1;
+
+        char* getScoreString();
     public:
         void drawOn(ScreenInterface* screen, Position position) override;
         void eraseFrom(ScreenInterface* screen, Position position) override;
